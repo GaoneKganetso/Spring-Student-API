a768dba91d3f12fcaa779cf05c19767eae5b0f59
Gaone Kganetso <gaonekganetso@gmail.com>
2021-11-28 16:51:44 +0200

    Created a student database in postgres and connected it

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-//Make class rest end point
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
